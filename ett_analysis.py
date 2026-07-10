ab9031c8f70e891317eca0f9ebf586940874f1e5
ahmet.koc <ahmet.koc@data-boss.com.tr>
2026-07-10 08:45:41 +0300

    Add new analysis scripts and results for ETT and tourism datasets

--- ett_analysis.py
+++ ett_analysis.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import pprint
 
-data = pd.read_pickle("sil/ALL_RESULT_ett_m2.pkl")
-print(data.keys())
+for ett_type in ["h1", "h2", "m1", "m2"]:
+    data = pd.read_pickle(f"ALL_RESULT_ett_{ett_type}.pkl")
+    print(f"{ett_type}: ")
+    pprint.pprint(data[ett_type])
+
